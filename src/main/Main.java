d0d01aa9bc9e3b070055321572794aaf5bf31027
himanshu1323235 <himanshu,gautam_cs18@gla.ac.in>
2019-08-22 15:40:14 +0530

    himanshu

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine().trim();
+        System.out.println("welcome"+ userName +"!");
+        System.out.println("ho gaya!");
+
+
+        scanner.close();
+    }
+}
